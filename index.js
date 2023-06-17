3f75756885680e2781605e8da1f5e1e7fd11a677
Yuriy <yfrolou@mail.ru>
2023-06-17 19:51:14 +0300

    Добавлена обработка аргументов при запуске: путь до директории и вложенность

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 const fs = require("fs");
 const clc = require('cli-color');
-const directoryName = "1";
+
 let fileCounter = 0;
 let dirCounter = 0;
-
+let nesting = process.argv[3];
+if (isNaN(+nesting) || Number(nesting) < 0) {
+    throw new Error("Второй аргумент должен быть положительным числом");
+}
 
 function showTreeDir(dir, depth = 0) {
+    if (depth >= nesting) {
+        return;
+    }
+
     let listDir = fs.readdirSync(dir);
     for (let item of listDir) {
-
         if (fs.statSync(dir + "/" + item).isDirectory()) {
             console.log("  ".repeat(depth) + "|__" + clc.blue(item));
             dirCounter++;
             showTreeDir(dir + "/" + item, depth + 1);
         } else {
-            console.log("  ".repeat(depth) +"-> "+ item);
+            console.log("  ".repeat(depth) + "-> " + item);
             fileCounter++;
         }
+
     }
-}
-function start(dir){
-    console.log(clc.blue(dir));
-    showTreeDir(dir);
-    console.log(`Directories: ${dirCounter}. Files: ${fileCounter}`);
+
 }
 
-start(directoryName);
+function start(dir) {
+    try {
+        console.log(clc.blue(dir));
+        showTreeDir(dir);
+        console.log(`Directories: ${dirCounter}. Files: ${fileCounter}`);
+    } catch {
+        console.log("Проверьте правильность аргументов при запуске скрипта))). Первый аргумент должен представлять путь до директории");
+    }
+}
+
+start(process.argv[2]);
